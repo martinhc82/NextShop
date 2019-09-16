797269a8c8a91d501411f378fd5007beecb14bf4
fedeheinemann <federicoheinemann@gmail.com>
2019-09-16 15:29:11 -0300

    Index.php navbar.php
    Armé navbar.php con código de login incrustado "modal". Index.php con 'require_once' navbar.php

---
+++ navbar.php
@@ -0,0 +1,105 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+  <a class="navbar-brand" href="Index.html"><img class=""src="img/logo-nextShop.svg"  width="150" alt="nextShop"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="Index.html">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Productos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Microprocesadores</a>
+          <a class="dropdown-item" href="#">Motherboards</a>
+          <a class="dropdown-item" href="#">Memorias RAM</a>
+          <a class="dropdown-item" href="#">Almacenamiento</a>
+          <a class="dropdown-item" href="#">Gabinetes</a>
+          <a class="dropdown-item" href="#">Fuentes</a>
+          <a class="dropdown-item" href="#">Refrigeración</a>
+          <a class="dropdown-item" href="#">Placas de Video</a>
+          <a class="dropdown-item" href="#">Periféricos</a>
+          <a class="dropdown-item" href="#">Monitores</a>
+          <a class="dropdown-item" href="#">Audio</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">CPUs Armadas</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="FAQ.html">Preguntas Frecuentes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Sucursales</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="Carrito.html"><i class="fas fa-shopping-cart"></i></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="modal" data-target="#formLoginPrueba"><i class="fas fa-user"></i></a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link"><i class="fab fa-facebook-square"></i></a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link"><i class="fab fa-instagram"></i></a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link"><i class="fab fa-twitter-square"></i></a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+
+<!-- Modal -->
+<div class="modal fade" id="formLoginPrueba" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Ingresa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+        <!-- Modal Formulario -->
+        <form class=""action="index.php" method="post">
+          <div class="form-group">
+            <label for="usu">E-mail:</label>
+            <input class="form-control" id="usu" type="email" placeholder="Ingrese su E-mail..." required="true">
+          </div>
+
+          <div class="form-group">
+            <label for="pass">Contraseña:</label>
+            <input class="form-control" id="pass" type="password" placeholder="Ingrese su contraseña..." required="true">
+          </div>
+
+
+
+      </div>
+      <div class="modal-footer">
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+        </div>
+
+        <br>
+
+        <input type="submit" class="btn btn-outline-secondary my-2 my-sm-0" value="Ingresar">
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
