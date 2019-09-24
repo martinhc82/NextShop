ac8d5bba64251997dc8520f34f46f26ae3f6bd6f
fedeheinemann <federicoheinemann@gmail.com>
2019-09-24 18:51:50 -0300

    Cambios footer.php

--- Index.php
+++ Index.php
@@ -171,27 +171,11 @@
 
   </div>
 
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <ul class="text-center text-white navbar-nav mr-auto">
-        <li> <a class="m-0 text-center text-white" href="FAQ.html">Preguntas Frecuentes</a></li>
-        <li><a class="m-0 text-center text-white" href="Contacto.html">Contacto</a></li>
-        <li class="nav-item">
-          <a href="#" class="m-0 text-center text-white"><i class="fab fa-facebook-square"></i></a>
-          <a href="#" class="m-0 text-center text-white"><i class="fab fa-instagram"></i></a>
-          <a href="#" class="m-0 text-center text-white"><i class="fab fa-twitter-square"></i></a>
-        </li>
-        <li><p class="m-0">Copyright &copy; nextShop S.R.L. 2019</p></li>
-      </ul>
+  <!-- Footer -->
 
-     </div>
-
-  </footer>
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <?php
+    require_once("footer.php");
+     ?>
 </body>
 
 </html>
