989f70a5fdf3bd61d75998f031e52bd24c2d3021
fedeheinemann <federicoheinemann@gmail.com>
2019-09-24 18:17:13 -0300

    faq "Centro de Ayuda"

---
+++ faq.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>nextShop</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="fontawesome/css/all.css">
+<link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+<link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Page Content -->
+
+<div class="container">
+
+    <!-- Barra de Navegación -->
+
+      <?php
+      require_once("navbar.php");
+       ?>
+
+</div>
+
+
+
+
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <ul class="text-center text-white navbar-nav mr-auto">
+        <li> <a class="m-0 text-center text-white" href="FAQ.html">Preguntas Frecuentes</a></li>
+        <li><a class="m-0 text-center text-white" href="Contacto.html">Contacto</a></li>
+        <li class="nav-item">
+          <a href="#" class="m-0 text-center text-white"><i class="fab fa-facebook-square"></i></a>
+          <a href="#" class="m-0 text-center text-white"><i class="fab fa-instagram"></i></a>
+          <a href="#" class="m-0 text-center text-white"><i class="fab fa-twitter-square"></i></a>
+        </li>
+        <li><p class="m-0">Copyright &copy; nextShop S.R.L. 2019</p></li>
+      </ul>
+
+
+
+     </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+<!-- <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
