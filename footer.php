7cb7ca483b03362296a5b609eb2b6ca08bc14ce0
fedeheinemann <federicoheinemann@gmail.com>
2019-10-01 17:31:42 -0300

    Productos
    Especificaciones Actualización

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <!-- Footer -->
 <footer class="py-5 bg-dark">
   <div class="container">
-    <ul class="text-center text-white navbar-nav mr-auto">
-      <li> <a class="m-0 text-center text-white" href="faq.php">Centro de Ayuda</a></li>
-      <li><a class="m-0 text-center text-white" href="contacto.php">Contacto</a></li>
+
+    <ul class="text-white navbar-nav mr-auto">
+      <li> <a class="m-0 text-white" href="faq.php">Centro de Ayuda</a></li>
+      <li><a class="m-0 text-white" href="contacto.php">Contacto</a></li>
       <li class="nav-item">
         <a href="#" class="m-0 text-center text-white"><i class="fab fa-facebook-square"></i></a>
         <a href="#" class="m-0 text-center text-white"><i class="fab fa-instagram"></i></a>
@@ -11,8 +12,6 @@
       </li>
       <li><p class="m-0">Copyright &copy; nextShop S.R.L. 2019</p></li>
     </ul>
-
-
 
    </div>
   <!-- /.container -->
---
+++ techSpecs-leno-330.php
@@ -0,0 +1,74 @@
+<ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Marca
+    <span class="badge badge-danger">LENOVO</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Modelo
+    <span class="badge badge-primary badge-pill">V330 CI5 4GB 1TB 15.6</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Garantía
+    <span class="badge badge-primary badge-pill">12 meses</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    ID Fabricante
+    <span class="badge badge-primary badge-pill">81AX000GAR</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Procesador
+    <span class="badge badge-primary badge-pill">Intel Core i5</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Velocidad del Procesador
+    <span class="badge badge-primary badge-pill">2.0 GHz</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Memoria RAM
+    <span class="badge badge-primary badge-pill">4 GB</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Tipo de Memoria RAM
+    <span class="badge badge-primary badge-pill">DDR 4</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Memoria RAM. Capacidad Máxima
+    <span class="badge badge-primary badge-pill">20 GB</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Resolución de la cámara frontal
+    <span class="badge badge-primary badge-pill">720 px</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Bluetooth
+    <span class="badge badge-primary badge-pill">Sí</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    HDMI
+    <span class="badge badge-primary badge-pill">Sí</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    USB 3.0
+    <span class="badge badge-primary badge-pill">Sí</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Wi-Fi
+    <span class="badge badge-primary badge-pill">Sí</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Tipo de Pantalla
+    <span class="badge badge-primary badge-pill">LED</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Tamaño de Pantalla
+    <span class="badge badge-primary badge-pill">15,6"</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Capacidad del Disco Rígido
+    <span class="badge badge-primary badge-pill">1 TB</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Sistema Operativo
+    <span class="badge badge-primary badge-pill">FreeDOS</span>
+  </li>
+</ul>
