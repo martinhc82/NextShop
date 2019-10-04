2ca022a433e8de4283f806256ade113e17ce0f4a
fedeheinemann <federicoheinemann@gmail.com>
2019-10-04 08:22:02 -0300

    Index 3
    Imagenes Enlaces

---
+++ navbarIzq-index-3.php
@@ -0,0 +1,23 @@
+<div class="list-group navbarIzq">
+  <table class="table table-sm table-dark">
+    <thead>
+      <tr>
+        <th scope="col">Dólar</th>
+        <th scope="col">Compra</th>
+        <th scope="col">Venta</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Bco. Nación</th>
+        <td>60</td>
+        <td>65</td>
+      </tr>
+      <tr>
+        <th scope="row">Blue</th>
+        <td>62</td>
+        <td>67</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
